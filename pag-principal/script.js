18a3f9d39e95a3718636c485ce6aca8c136ccab6
Gabriel Lippi <gabriellippidacosta@gmail.com>
2025-01-30 19:31:12 -0300

    melhorando a pag principal

---
+++ pag-principal/script.js
@@ -0,0 +1,8 @@
+function clicar(){
+    var itens = window.document.getElementById('itens')
+    if (itens.style.display == 'block') {
+        itens.style.display = 'none'
+    }else {
+        itens.style.display = 'block'
+    }
+}
