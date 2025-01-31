a55dabf13aee69f751415f18733598efb110ee8c
Gabriel Lippi <gabriellippidacosta@gmail.com>
2025-01-31 14:47:54 -0300

    melhorando o design da pag principal

--- pag-principal/script.js
+++ pag-principal/script.js
@@ -1,8 +1,4 @@
-function clicar(){
-    var itens = window.document.getElementById('itens')
-    if (itens.style.display == 'block') {
-        itens.style.display = 'none'
-    }else {
-        itens.style.display = 'block'
-    }
+function clicar() {
+    var menu = document.getElementById('itens');
+    menu.classList.toggle('active');
 }
